768c7f239ce4e0e8c2bad2adf65956de28e045cb
Jashanpreet Kaur <jashanpreetkaur24@navgurukul.org>
2026-01-12 18:00:23 +0530

    package files changed

--- my-portfolio/vite.config.js
+++ my-portfolio/vite.config.js
@@ -2,10 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/react-vite'
 import tailwindcss from '@tailwindcss/vite'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
   ],
 })
-
